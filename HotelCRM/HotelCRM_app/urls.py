9551f24f51680545e880311c5f94ffb567e70ad0
Mortykemz <clah8413@gmail.com>
2024-03-02 19:25:23 +0500

    123

---
+++ HotelCRM/HotelCRM_app/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+    path('booking', views.booking, name='booking'),
+    path('contact', views.contact, name='contact'),
+    path('destination', views.destination, name='destination'),
+    path('home', views.home, name='home'),
+    path('package', views.package, name='package'),
+    path('service', views.service, name'service'),
+    path('team', views.team, name='team'),
+    path('testimonial', views.testimonial name='testimonial'),
+    
+]
--- HotelCRM/HotelCRM_app/views.py
+++ HotelCRM/HotelCRM_app/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def about(request)
+    return render(request, 'about.html')
+
+def booking(request)
+    return render(request, 'booking.html')
+
+def contact(request)
+    return render(request, 'contact.html')
+    
+def destination(request)
+    return render(request, 'destination.html')
+
+def home(request)
+    return render(request, 'home.html')
+        
+def package(request)
+    return render(request, 'package.html')
+
+def service(request)
+    return render(request, 'service.html')
+
+def team(request)
+    return render(request, 'team.html')
+
+def testimonial(request)
+    return render(request, 'testimonial.html')
